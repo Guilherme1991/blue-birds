b66507c6a7a5933c440d4dfcf57f3a8b706f4b46
Guilherme Luiz Quintino Da Silva <32807543+Guilherme1991@users.noreply.github.com>
2019-07-19 21:48:39 -0300

    upload archives
    upload archives

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let icon = document.querySelector('.hamburguer img')
+let oculto = document.querySelector('header')
+let campo = document.querySelector('#form')
+let campoTwo = document.querySelector('#form-2')
+
+icon.addEventListener('click', function(){
+    oculto.classList.toggle('ative')
+})
+
+
+
+function enviar(){
+   // if(campo.value && campoTwo.value ===''){
+        //this.setAttribute('style', 'border: 2px solid red')
+        alert('In development!')
+   // }
+}
